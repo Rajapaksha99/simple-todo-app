87a2b62808ed1509b8e897a1fd00f81177436925
Rajapaksha99 <rmkrr99@gmail.com>
2025-07-30 21:45:11 +0530

    ss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>To</h1>
+        <h1>Todd</h1>
         <div className="top">
           <input
             type="text"
