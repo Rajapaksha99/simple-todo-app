702c1d2bf41165a95a9105d2dcd7b20734ac57d3
Rajapaksha99 <rmkrr99@gmail.com>
2025-07-30 15:11:36 +0530

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>ToDo App</h1>
+        <h1>ToDo Apps</h1>
         <div className="top">
           <input
             type="text"
